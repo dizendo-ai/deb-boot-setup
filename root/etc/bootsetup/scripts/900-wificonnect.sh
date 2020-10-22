8052f21fbd7aeff7ad9008b7ecdaa16c9e5b335a
abnerjacobsen <abner@apoana.com.br>
2020-10-22 17:41:21 -0300

    Force the use o WIFI country to correct find wifi 5G networks.

--- root/etc/bootsetup/scripts/900-wificonnect.sh
+++ root/etc/bootsetup/scripts/900-wificonnect.sh
@@ -4,12 +4,28 @@
 #	Configure WIFI if cann't reach google.com
 #
 
-# Check if can access google.com
-/usr/bin/wget --spider https://google.com 2>&1
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket
 
-if [ $? -eq 0 ]; then
-    printf 'Skipping WiFi Connect\n'
+WIFICOUNTRY_FILE=$BOOTSETUP_CONFIG/wifi_country
+
+# Check if config file exists
+if [ ! -f "$WIFICOUNTRY_FILE" ]; then
+	wifi_country="$(cat $WIFICOUNTRY_FILE)"
 else
-    printf 'Starting WiFi Connect\n'
-    /usr/local/sbin/wifi-connect --portal-ssid="$HOSTNAME"
+    wifi_country="BR"
 fi
+
+# Check if can access google.com
+echo "bootsetup: Begin test connection to https://google.com..."
+if ping -c 3 8.8.8.8 > /dev/null; then
+	echo "bootsetup: Have internet connection"
+else
+	echo "bootsetup: Start WIFI connection setup"
+	sudo iw reg set "$wifi_country"
+	sleep 2
+	sudo rfkill unblock all
+	sleep 2
+	sudo /usr/local/sbin/wifi-connect --portal-ssid="$HOSTNAME"
+	echo "bootsetup: End WIFI connection setup"
+fi
+
